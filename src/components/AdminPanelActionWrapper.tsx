7d6b834b7ae10ede2b45be1476f526fd932dede8
ninadarbaidze <ninadarbaidze66@gmail.com>
2022-07-01 15:08:03 +0400

    feat: populate band members from database

--- src/components/AdminPanelActionWrapper.tsx
+++ src/components/AdminPanelActionWrapper.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Children } from 'components';
 
 type AdminPanelWrapper = {
   children: React.ReactNode;
--- src/pages/BandMember/BandMember.tsx
+++ src/pages/BandMember/BandMember.tsx
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { AdminPanelActionWrapper } from 'components';
 import { Member } from 'pages/BandMember/components';
 import { Outlet, NavLink } from 'react-router-dom';
+import axios from 'axios';
+
+export type Data = {
+  id: string;
+  name: string;
+  instrument: string;
+  orbitLength: number;
+  color: string;
+  biography: string;
+};
 
 const BandMember = () => {
+  const [data, setData] = useState<Data[]>([]);
+
+  const token = localStorage.getItem('token');
+  const fetchData = useCallback(async () => {
+    try {
+      const { data: response } = await axios.get(
+        'http://localhost:3000/band-members',
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      setData(response);
+    } catch (error: any) {}
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+    console.log(data);
+  }, [fetchData]);
+
   return (
     <>
       <AdminPanelActionWrapper header='ჯგუფის წევრები'>
         <div className='flex gap-10'>
-          <Member name={'ნინა'} />
-          <Member name={'ნინა'} />
-          <Member name={'ნინა'} />
+          {data.map((data) => (
+            <Member name={data.name} />
+          ))}
         </div>
         <button className=' text-link font-bold underline mt-[-20px]'>
           <NavLink to='new-member'>ახალი წევრი გვყავს?</NavLink>
