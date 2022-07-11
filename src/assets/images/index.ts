d58caccf809f90730947e1cdf362d57e7f2e108d
ninadarbaidze <ninadarbaidze66@gmail.com>
2022-07-11 12:06:09 +0400

    chore: change image names to pascal case

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,5 +1,5 @@
-export { default as memberIcon } from './memberIcon.png';
+export { default as memberIcon } from './member-icon.png';
 export { default as youtube } from './youtube.png';
 export { default as band } from './band.jpg';
-export { default as BandLogo } from './BandLogo.png';
+export { default as BandLogo } from './band-logo.png';
 export { default as sun } from './sun.png';
