3aa856b89cc524df2f8f41040ca2811d316e53e5
ninadarbaidze <ninadarbaidze66@gmail.com>
2022-07-11 14:05:22 +0400

    fix: env domain instead of localhost

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -25,7 +25,7 @@ const Modal: React.FC<ModalDataMembers> = (props) => {
             <img
               src={
                 props.image.length > 0
-                  ? `http://localhost:3000/${props.image[0].imageUrl}`
+                  ? `${process.env.REACT_APP_DOMAIN}/${props.image[0].imageUrl}`
                   : memberIcon
               }
               alt=''
