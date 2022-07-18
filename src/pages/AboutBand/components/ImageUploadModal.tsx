d3a874166c1fa2c0cf31bb2b18aa63649929b0e9
ninadarbaidze <ninadarbaidze66@gmail.com>
2022-07-18 19:23:12 +0400

    feat: add background to band logo upload

--- src/pages/AboutBand/components/ImageUploadModal.tsx
+++ src/pages/AboutBand/components/ImageUploadModal.tsx
@@ -68,7 +68,7 @@ const ImageUploadModal: React.FC<ImageUploadBand> = (props) => {
         <div className='flex flex-col justify-between w-[80%] mb-4 items-center ml-[50%] mr-[50%] border-b-[1px] border-dark50 h-6 '></div>
         <div className='flex flex-col items-center gap-10 2xl:gap-20'>
           <div className='flex flex-col items-center mt-4 2xl:mt-16 w-[30rem]'>
-            <div className='flex flex-col justify-center items-center py-16 '>
+            <div className='flex flex-col justify-center items-center py-16 rounded-full bg-backdrop border-[3px]  mb-6 border-white w-[15rem] h-[15rem] 2xl:w-[20rem] 2xl:h-[20rem] drop-shadow-5xl'>
               <img
                 src={imagePreviewHandler()}
                 alt='band-icon'
